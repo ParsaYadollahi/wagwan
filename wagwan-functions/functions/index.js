bbf1109d5b18269061eec64f06d48c5add17c7de
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2019-12-24 11:57:13 -0500

    Switching to express

--- wagwan-functions/functions/index.js
+++ wagwan-functions/functions/index.js
@@ -3,32 +3,39 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(); //function already knows that .firebaserc is  id of app, so pass nothing
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const express = require('express');
+const app = express();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello World!");
 });
 
-
-// get data
+////////////////////////////////////////////////////////////////////////////
+//     GET without express
+////////////////////////////////////////////////////////////////////////////
+// get data WITHOUT EXPRESS
 exports.getScreams = functions.https.onRequest((req, resp) => {
     admin // Can check out more imports from firebase.get-data
         .firestore()
-        .collection('screams')
+        .collection('screams') // get elements at 'screams' in db
         .get()
-        .then((data) => {
-            let screams = [];
+        .then((data) => { // promise 
+            let screams = []; // initialize array to add screams to it
             data.forEach((doc) => { // Data is a collection of elements, looping thru them all and adding them to an Array
                 screams.push(doc.data()); // adding to list
             });
-            return resp.json(screams);
+            return resp.json(screams); // respond with array
         })
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err)); // catch any error and print it
 });
+////////////////////////////////////////////////////////////////////////////
 
-// create data
+// create data | will update in real time (post req)
 exports.createScream = functions.https.onRequest((req, resp) => {
+    // catch case that sending get req to link for  post req
+    if (req.method !== 'POST') {
+        return resp.status(400).json({ error: 'My guy, you sending a get request to a post method'});
+    }
     const new_Scream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
@@ -37,13 +44,13 @@ exports.createScream = functions.https.onRequest((req, resp) => {
 
     admin
         .firestore()
-        .collection('screams')
+        .collection('screams') // like python dictionary, get elements at screams
         .add(new_Scream) // takes json obj and adds it to database (one above)
         .then(doc => { //returns a promise | If here means weve made a new docs
-            resp.json({message: `document ${doc.id} created successfully`});
+            resp.json({ message: `document ${doc.id} created successfully`}); // respond with when successfully did post req
         })
-        .catch(err => {
-            resp.status(500).json({error: 'something went wrong fam'});
+        .catch((err) => {
+            resp.status(500).json({error: 'something went wrong fam'}); // respond a status of 500 and give message (server error)
             console.error(err);
         });
 });
