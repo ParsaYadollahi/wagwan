a6bb109ad2773560c428180c5017ff2d89a11cee
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2019-12-24 13:02:21 -0500

    Added express requests and response

---
+++ wagwan-functions/functions/dbschema.js
@@ -0,0 +1,13 @@
+// This is a file to show what the data will look like (the schema)
+let db =  {
+    screams: [
+        {
+            userHandle: 'the user of the screamer',
+            body: 'the body of the screamer',
+            createdat: 'String representing the date of the req',
+            likeCount: 5,
+            commentCount: 22
+        }
+
+    ]
+}
--- wagwan-functions/functions/index.js
+++ wagwan-functions/functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const fbfunctions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp(); //function already knows that .firebaserc is  id of app, so pass nothing
@@ -6,7 +6,7 @@ admin.initializeApp(); //function already knows that .firebaserc is  id of app, 
 const express = require('express');
 const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = fbfunctions.https.onRequest((request, response) => {
  response.send("Hello World!");
 });
 
@@ -14,32 +14,77 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 //     GET without express
 ////////////////////////////////////////////////////////////////////////////
 // get data WITHOUT EXPRESS
-exports.getScreams = functions.https.onRequest((req, resp) => {
+// exports.getScreams = fbfunctions.https.onRequest((req, resp) => {
+    // admin // Can check out more imports from firebase.get-data
+    //     .firestore()
+    //     .collection('screams') // get elements at 'screams' in db
+    //     .get()
+    //     .then((data) => { // promise 
+    //         let screams = []; // initialize array to add screams to it
+    //         data.forEach((doc) => { // Data is a collection of elements, looping thru them all and adding them to an Array
+    //             screams.push(doc.data()); // adding to list
+    //         });
+    //         return resp.json(screams); // respond with array
+    //     })
+    // .catch((err) => console.error(err)); // catch any error and print it
+// });
+////////////////////////////////////////////////////////////////////////////
+//     POST without express
+////////////////////////////////////////////////////////////////////////////
+
+// // create data | will update in real time (post req)
+// exports.createScream = fbfunctions.https.onRequest((req, resp) => {
+//     // catch case that sending get req to link for  post req
+//     if (req.method !== 'POST') {
+//         return resp.status(400).json({ error: 'My guy, you sending a get request to a post method'});
+//     }
+//     const new_Scream = {
+//         body: req.body.body,
+//         userHandle: req.body.userHandle,
+//         createAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin
+//         .firestore()
+//         .collection('screams') // like python dictionary, get elements at screams
+//         .add(new_Scream) // takes json obj and adds it to database (one above)
+//         .then(doc => { //returns a promise | If here means weve made a new docs
+//             resp.json({ message: `document ${doc.id} created successfully`}); // respond with when successfully did post req
+//         })
+//         .catch((err) => {
+//             resp.status(500).json({error: 'something went wrong fam'}); // respond a status of 500 and give message (server error)
+//             console.error(err);
+//         });
+// });
+
+
+app.get('/screams', (req, resp) => { 
     admin // Can check out more imports from firebase.get-data
         .firestore()
         .collection('screams') // get elements at 'screams' in db
+        .orderBy('createAt', 'desc') // order the output by the keyword "createdat"
         .get()
         .then((data) => { // promise 
             let screams = []; // initialize array to add screams to it
             data.forEach((doc) => { // Data is a collection of elements, looping thru them all and adding them to an Array
-                screams.push(doc.data()); // adding to list
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createAt: doc.data().createAt
+                }); // adding to list
             });
             return resp.json(screams); // respond with array
         })
     .catch((err) => console.error(err)); // catch any error and print it
-});
-////////////////////////////////////////////////////////////////////////////
+})
 
-// create data | will update in real time (post req)
-exports.createScream = functions.https.onRequest((req, resp) => {
-    // catch case that sending get req to link for  post req
-    if (req.method !== 'POST') {
-        return resp.status(400).json({ error: 'My guy, you sending a get request to a post method'});
-    }
+app.post('/screams', (req, resp) => { // takes in path and handler
+    // DONT need to catch if sending a get to a post method, express takes care of that
     const new_Scream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createAt: admin.firestore.Timestamp.fromDate(new Date())
+        createAt: new Date().toISOString()
     };
 
     admin
@@ -53,4 +98,9 @@ exports.createScream = functions.https.onRequest((req, resp) => {
             resp.status(500).json({error: 'something went wrong fam'}); // respond a status of 500 and give message (server error)
             console.error(err);
         });
-});
+})
+
+// Good practice to have :
+// https://baseurl.com/api/.....
+// By doing exports, we can import the api function
+exports.api = fbfunctions.https.onRequest(app); // this will give is the extension
