b962aae1ecfd67782b1941c6bd67a48a51d06516
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-01-02 15:30:36 -0500

    Added basic react and set up from end

---
+++ wagwan-client/src/pages/signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class signup extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Signup page
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default signup
